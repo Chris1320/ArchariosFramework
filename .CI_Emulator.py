966950a6d05096e880379e4fbf948d5af4c0181d
Catayao56 <Catayao56@gmail.com>
2018-09-24 18:48:49 +0800

    Updated CI Emulator
    Signed-off-by: Catayao56 <Catayao56@gmail.com>

--- .CI_Emulator.py
+++ .CI_Emulator.py
@@ -165,6 +165,22 @@ class testEnvLinux:
                 """
                 print()
 
+        self.success_percentage = 0
+        no_of_jobs = len(self.report)
+        one = 100 / no_of_jobs
+        for result in self.report:
+            if result[0] == 0:
+                self.success_percentage += one
+
+            else:
+                pass
+
+        if self.success_percentage < 0 or self.success_percentage > 100:
+            print(self.CGR + "Error calculating pipeline success result!")
+
+        else:
+            print(self.CGR + "Pipeline Result: {0}% success (over 100%)".format(
+                self.success_percentage) + self.END)
         print()
         return(retcode)
 
